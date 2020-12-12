58a8a7fc4e7bfffe0f6aa5a865768cbafa82f7b8
Daniel Sangelo <danielsangelo@hotmail.com>
2020-12-12 10:45:56 -0300

    initial commit

---
+++ IES/Controllers/InstitutionController1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using IES.Models;
+
+namespace IES.Controllers
+{
+    public class InstitutionController1 : Controller
+    {
+        private static IList<Institution> _institutions = new List<Institution>()
+        {
+            new Institution()
+            {
+                InstitutionId = 1,
+                Name = "UniParaná",
+                Endereco="Paraná"
+            },
+            new Institution()
+            {
+                InstitutionId = 2,
+                Name = "UniSanta",
+                Endereco="Santa Catarina"
+            },
+            new Institution()
+            {
+                InstitutionId = 3,
+                Name = "UniSãoPaulo",
+                Endereco="São Paulo"
+            },
+            new Institution()
+            {
+                InstitutionId = 4,
+                Name = "UniSulgrandense",
+                Endereco="Rio Grande do Sul"
+            },
+            new Institution()
+            {
+                InstitutionId = 5,
+                Name = "UniCarioca",
+                Endereco="Rio de Janeiro"
+            },
+            new Institution()
+            {
+                InstitutionId = 6,
+                Name = "UniHorizonte",
+                Endereco="Minas Gerais"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_institutions);
+        }
+    }
+}
---
+++ IES/Models/Institution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IES.Models
+{
+    public class Institution
+    {
+        public long? InstitutionId { get; set; }
+        public string Name { get; set; }
+        public string Endereco { get; set; }
+    }
+}
