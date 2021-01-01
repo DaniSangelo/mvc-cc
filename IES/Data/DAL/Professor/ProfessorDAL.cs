6b9a2ae7ebded61c7b9bd6d7682dcb1d688a3c93
Daniel Sangelo <danielsangelo@hotmail.com>
2021-01-01 17:53:51 -0300

    DAL's Professor and Course was created

---
+++ IES/Data/DAL/Professor/ProfessorDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IES.Data.DAL.Professor
+{
+    public class ProfessorDAL
+    {
+        private readonly IESContext _context;
+
+        public ProfessorDAL(IESContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ IES/Data/DAL/Registrations/CourseDAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IES.Data.DAL.Registrations
+{
+    public class CourseDAL
+    {
+    }
+}
